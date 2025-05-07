c4c7b9d1c92150796c0f3407046335ff488a2bac
SzymonLabedziewski <slabedziewski01@gmail.com>
2025-05-07 21:28:39 +0200

    Update zestaw04.py

--- zestaw04.py
+++ zestaw04.py
@@ -1,9 +1,14 @@
 from MyGraph import *
 from DrawGraph import *
 from DirectedMyGraph import *
-import random
+from Algorithms import *
+# from DirectedDrawGraph import *
 
-# Przykładowe użycie:
+import random
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
 if __name__ == "__main__":
     # Zadanie 1
     # Napisać program do kodowania grafów skierowanych (digrafów) i do generowania losowych digrafów z zespołu G(np).
@@ -21,9 +26,6 @@ if __name__ == "__main__":
     print(g_inc)
 
 
-    import networkx as nx
-    import matplotlib.pyplot as plt
-
     # konwersja do NetworkX DiGraph
     G = nx.DiGraph()
     for u in range(g_list.n):
@@ -39,4 +41,22 @@ if __name__ == "__main__":
             font_weight="bold", font_size=10)
     plt.title(f"Digraf G({n}, {p})")
     plt.axis("off")
+    # plt.show()
+
+    # Zadanie 2
+    # Zaimplementować algorytm Kosaraju do szukania silnie spójnych skła dowych na digrafie i zastosować go do digrafu losowego
+
+    # Przykładowe użycie: wygeneruj losowy graf i zastosuj algorytm
+    # g_list = generate_random_directed_graph_by_probability(n, p)
+    comp_map = kosaraju(g_list)
+
+    # # Grupowanie wierzchołków według numeru składowej
+    components: dict[int, list[int]] = {}
+    for vertex, cid in comp_map.items():
+        components.setdefault(cid, []).append(vertex)
+    print()
+    print("Liczba silnie spójnych składowych:", len(components))
+    for cid, vertices in components.items():
+        print(f"Składowa {cid}: {sorted(vertices)}")
+    
     plt.show()
